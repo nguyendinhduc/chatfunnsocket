faa1b7511b4230ae6eb5cc292cec4b6545823c78
Dinh-Duc Nguyen <ducn.nguyen@vmodev.com>
2018-09-15 12:59:36 +0700

    make it better

---
+++ src/main/java/com/ducnd/chatsocket/Main.java
@@ -0,0 +1,18 @@
+package com.ducnd.chatsocket;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@PropertySource("classpath:application.properties")
+@EnableScheduling
+@EnableAsync(proxyTargetClass = true)
+@EnableCaching
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
