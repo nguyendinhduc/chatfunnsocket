bfa3e96d2c66be9dc3753632947cdca06929ad4e
Dinh-Duc Nguyen <ducn.nguyen@vmodev.com>
2018-09-18 22:44:54 +0700

    recommit

--- src/main/java/com/ducnd/chatsocket/Main.java
+++ src/main/java/com/ducnd/chatsocket/Main.java
@@ -24,7 +24,7 @@ public class Main {
     public static void main(String[] args) {
 
 //        SpringApplication.run(Main.class, args);
-        initSocket(8080);
+        initSocket(5000);
     }
 
     private static void initSocket(int thePort) {
