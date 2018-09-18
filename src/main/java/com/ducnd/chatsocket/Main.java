dedd69b200319a59ea518ed3c372fec7ef630765
Dinh-Duc Nguyen <ducn.nguyen@vmodev.com>
2018-09-18 22:41:51 +0700

    change port

--- src/main/java/com/ducnd/chatsocket/Main.java
+++ src/main/java/com/ducnd/chatsocket/Main.java
@@ -1,11 +1,16 @@
 package com.ducnd.chatsocket;
 
-import org.springframework.boot.SpringApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
@@ -13,8 +18,34 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync(proxyTargetClass = true)
 @EnableCaching
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    private static ServerSocket serverSocket;
+
     public static void main(String[] args) {
 
-        SpringApplication.run(Main.class, args);
+//        SpringApplication.run(Main.class, args);
+        initSocket(8080);
+    }
+
+    private static void initSocket(int thePort) {
+        if (serverSocket != null) {
+            return;
+        }
+        try {
+            serverSocket = new ServerSocket(thePort);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    socket.close();
+                    LOG.info("initSocket close socket............");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
