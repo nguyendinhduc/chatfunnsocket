9e508b0353d06d8925d205d9d85212dcf9c1270a
Dinh-Duc Nguyen <ducn.nguyen@vmodev.com>
2018-09-18 22:51:06 +0700

    recommit

--- src/main/java/com/ducnd/chatsocket/TestMain.java
+++ src/main/java/com/ducnd/chatsocket/TestMain.java
@@ -1,16 +1,16 @@
-//package com.ducnd.chatsocket;
-//
-//import java.io.IOException;
-//import java.net.Socket;
-//
-//public class TestMain {
-//    public static void main(String[] args) {
-//        try {
-//            Socket socket = new Socket("https://chatfunnsocket.herokuapp.com", 5000);
-//            System.out.println("Connected");
-//            socket.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package com.ducnd.chatsocket;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class TestMain {
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("https://chatfunnsocket.herokuapp.com", 5000);
+            System.out.println("Connected");
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
